9b8dbf7bef4bedbb24fe4f4a0a274ad42948d0c5
Enrique Ramos <kikeramos.1994@gmail.com>
2023-11-17 20:20:31 -0600

    Funcionalidad de Login en MainActivity

--- app/src/main/java/com/enrique/listusers/MainActivity.java
+++ app/src/main/java/com/enrique/listusers/MainActivity.java
@@ -1,15 +1,24 @@
 package com.enrique.listusers;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
         AccederLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //FUNCION LOGIN
+                //comvertir a string los editext de correo y password
+                String correo = CorreoLogin.getText().toString();
+                String pass = PasswordLogin.getText().toString();
+
+                LogeoUsuarios(correo, pass);
             }
         });
 
@@ -48,5 +61,47 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RegisterUsers.class));
             }
         });
+    }
+
+    //METODO DE LOGIN
+    private void LogeoUsuarios(String correo, String pass) {
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        firebaseAuth.signInWithEmailAndPassword(correo, pass)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //Si el inicio de sesion fue exitoso
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(MainActivity.this, UserList.class));
+                            Toast.makeText(MainActivity.this, "Bienvenido/a " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                            finishAffinity();
+                        } else {
+                            progressDialog.dismiss();
+                            UsuarioInvalido();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        UsuarioInvalido();
+                    }
+                });
+    }
+
+    private void UsuarioInvalido() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(false);
+        builder.setTitle("¡HA OCURRIDO UN ERROR!");
+        builder.setMessage("Verifique si el correo o password sea los correctos")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
     }
 }
